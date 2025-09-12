b206d423783b0c84e9b4f141c0a00d8c4d267343
Zambrinn <zambrinfilho@gmail.com>
2025-09-12 15:48:16 -0300

    feat: refactor Todo and User models to use UUID for ID and establish relationship between Todo and User

--- src/main/java/br/com/todolist/aws_lambda_todo/model/Todo.java
+++ src/main/java/br/com/todolist/aws_lambda_todo/model/Todo.java
@@ -3,11 +3,15 @@ package br.com.todolist.aws_lambda_todo.model;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,11 +20,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Table(name = "todo")
+@Builder
 public class Todo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String description;
     private boolean done = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false) 
+    private User user;
 }
--- src/main/java/br/com/todolist/aws_lambda_todo/model/User.java
+++ src/main/java/br/com/todolist/aws_lambda_todo/model/User.java
@@ -12,16 +12,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "users")
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(unique = true, nullable = false)
