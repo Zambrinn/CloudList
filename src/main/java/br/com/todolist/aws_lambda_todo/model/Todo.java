aaabd0de330b648b599882dfea0f4744fc161d17
Zambrinn <zambrinfilho@gmail.com>
2025-09-12 11:59:01 -0300

    feat: refactor Todo model to use UUID for ID and add User model with username and password fields

--- src/main/java/br/com/todolist/aws_lambda_todo/model/Todo.java
+++ src/main/java/br/com/todolist/aws_lambda_todo/model/Todo.java
@@ -1,21 +1,25 @@
 package br.com.todolist.aws_lambda_todo.model;
 
+import java.util.UUID;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Table(name = "todo")
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
     private String description;
     private boolean done = false;
---
+++ src/main/java/br/com/todolist/aws_lambda_todo/model/User.java
@@ -0,0 +1,33 @@
+package br.com.todolist.aws_lambda_todo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Todo> todos = new ArrayList<>();
+}
