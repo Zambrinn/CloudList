7af1a5eb741da93ef3d2adc244b6f9657023499a
Zambrinn <zambrinfilho@gmail.com>
2025-09-11 21:10:44 -0300

    feat: add validation annotation to TodoRequestDTO and organize imports in Todo model

--- src/main/java/br/com/todolist/aws_lambda_todo/dto/TodoRequestDTO.java
+++ src/main/java/br/com/todolist/aws_lambda_todo/dto/TodoRequestDTO.java
@@ -1,9 +1,11 @@
 package br.com.todolist.aws_lambda_todo.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class TodoRequestDTO {
+    @NotBlank(message = "A descrição não pode estar em branco")
     private String description;
     private boolean done;
 }
--- src/main/java/br/com/todolist/aws_lambda_todo/model/Todo.java
+++ src/main/java/br/com/todolist/aws_lambda_todo/model/Todo.java
@@ -1,6 +1,9 @@
 package br.com.todolist.aws_lambda_todo.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
