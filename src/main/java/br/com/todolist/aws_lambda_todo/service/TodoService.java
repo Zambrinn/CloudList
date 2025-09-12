c3e6f272a6391616032773dc96e0ed5e825952c9
Zambrinn <zambrinfilho@gmail.com>
2025-09-12 11:58:46 -0300

    feat: update TodoService to use UUID for ID parameters and add UserService class

--- src/main/java/br/com/todolist/aws_lambda_todo/service/TodoService.java
+++ src/main/java/br/com/todolist/aws_lambda_todo/service/TodoService.java
@@ -1,6 +1,7 @@
 package br.com.todolist.aws_lambda_todo.service;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,43 +20,41 @@ public class TodoService {
 
     public TodoResponseDTO create(TodoRequestDTO requestDTO) {
         Todo todo = new Todo();
-        todo.setDescription(requestDTO.getDescription());
-        todo.setDone(requestDTO.isDone());
+        todo.setDescription(requestDTO.description());
+        todo.setDone(requestDTO.done());
         Todo savedTodo = todoRepository.save(todo);
 
         return convertToResponseDTO(savedTodo);
-        
+
     }
 
     public List<TodoResponseDTO> listAll() {
         return todoRepository.findAll()
-            .stream()
-            .map(this::convertToResponseDTO)
-            .collect(Collectors.toList());
+                .stream()
+                .map(this::convertToResponseDTO)
+                .collect(Collectors.toList());
     }
 
-    public TodoResponseDTO update(Long id, TodoRequestDTO todoRequestDTO) {
+    public TodoResponseDTO update(UUID id, TodoRequestDTO todoRequestDTO) {
         Todo existingTodo = todoRepository.findById(id)
-        .orElseThrow(() -> new ResourceNotFoundException("Item não encontrado com o ID: " + id));
+                .orElseThrow(() -> new ResourceNotFoundException("Item não encontrado com o ID: " + id));
 
-        existingTodo.setDescription(todoRequestDTO.getDescription());
-        existingTodo.setDone(todoRequestDTO.isDone());
+        existingTodo.setDescription(todoRequestDTO.description());
+        existingTodo.setDone(todoRequestDTO.done());
         Todo updatedTodo = todoRepository.save(existingTodo);
         return convertToResponseDTO(updatedTodo);
     }
 
-    public void delete(Long id) {
+    public void delete(UUID id) {
         if (!todoRepository.existsById(id)) {
             throw new ResourceNotFoundException("Item não encontrado com o id: " + id);
         }
     }
 
     private TodoResponseDTO convertToResponseDTO(Todo todo) {
-        TodoResponseDTO responseDTO = new TodoResponseDTO();
-        responseDTO.setId(todo.getId());
-        responseDTO.setDescription(todo.getDescription());
-        responseDTO.setId(todo.getId());
-
-        return responseDTO;
+        return new TodoResponseDTO(
+                todo.getId(),
+                todo.getDescription(),
+                todo.isDone());
     }
 }
---
+++ src/main/java/br/com/todolist/aws_lambda_todo/service/UserService.java
@@ -0,0 +1,16 @@
+package br.com.todolist.aws_lambda_todo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.todolist.aws_lambda_todo.repository.UserRepository;
+
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    
+
+    
+    
+}
