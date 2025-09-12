45c904809626715f793d25394197822e89f28898
Zambrinn <zambrinfilho@gmail.com>
2025-09-12 15:48:23 -0300

    feat: add user_id field to TodoRequestDTO and clean up imports in TodoRepository

--- src/main/java/br/com/todolist/aws_lambda_todo/dto/TodoRequestDTO.java
+++ src/main/java/br/com/todolist/aws_lambda_todo/dto/TodoRequestDTO.java
@@ -1,8 +1,11 @@
 package br.com.todolist.aws_lambda_todo.dto;
 
+import java.util.UUID;
+
 import jakarta.validation.constraints.NotBlank;
 
 public record TodoRequestDTO(
         @NotBlank(message = "A descrição não pode estar em branco") String description,
-        boolean done) {
+        boolean done,
+        UUID user_id) {
 }
--- src/main/java/br/com/todolist/aws_lambda_todo/repository/TodoRepository.java
+++ src/main/java/br/com/todolist/aws_lambda_todo/repository/TodoRepository.java
@@ -1,11 +1,11 @@
 package br.com.todolist.aws_lambda_todo.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.todolist.aws_lambda_todo.model.Todo;
-
-import java.util.UUID;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, UUID> {
