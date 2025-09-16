b079c974c4fd29c2e26cb1b9cabf4528044b5a54
Zambrinn <zambrinfilho@gmail.com>
2025-09-16 12:12:49 -0300

    fix: remove requestBody from deletemapping

--- src/main/java/br/com/todolist/aws_lambda_todo/controller/TodoController.java
+++ src/main/java/br/com/todolist/aws_lambda_todo/controller/TodoController.java
@@ -42,7 +42,7 @@ public class TodoController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable UUID id, @RequestBody TodoRequestDTO requestDTO) {
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
         todoService.delete(id);
         return ResponseEntity.noContent().build();
     }
