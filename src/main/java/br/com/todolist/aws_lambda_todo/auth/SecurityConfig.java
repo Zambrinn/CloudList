8a00deca76e71ae19c18168c02ef2bc5c54defc0
Zambrinn <zambrinfilho@gmail.com>
2025-09-24 14:32:32 -0300

    fix: update CORS configuration to allow requests from Vercel frontend

--- src/main/java/br/com/todolist/aws_lambda_todo/auth/SecurityConfig.java
+++ src/main/java/br/com/todolist/aws_lambda_todo/auth/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
         "http://localhost:5173",
         "http://localhost:5174",
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://cloud-list-frontend-z7cc.vercel.app"
     ));
 
 
